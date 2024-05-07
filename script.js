5c13a3f2a90449ff4d0ef7e8844b08e65826b33f
Albert Watbin <130539740+albert2Moon@users.noreply.github.com>
2024-05-07 04:45:12 +0300

    .

--- script.js
+++ script.js
@@ -18,25 +18,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the button
-    var toTopBtn = document.getElementById("toTopBtn");
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Get the button
+//     var toTopBtn = document.getElementById("toTopBtn");
 
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            toTopBtn.style.display = "block";
-        } else {
-            toTopBtn.style.display = "none";
-        }
-    };
-});
+//     // When the user scrolls down 20px from the top of the document, show the button
+//     window.onscroll = function () {
+//         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//             toTopBtn.style.display = "block";
+//         } else {
+//             toTopBtn.style.display = "none";
+//         }
+//     };
+// });
 
-// Function to scroll to the top of the document
-function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-}
+// // Function to scroll to the top of the document
+// function scrollToTop() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+// }
 
 
 
